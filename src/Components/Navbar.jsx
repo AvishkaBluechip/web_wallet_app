8a4705f6f2a57b52057c981aa2cdecc96e1203c6
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-09-17 14:40:08 +0530

    update
    nav bar slide bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,9 +5,10 @@ import user from '../assets/user-1.png'
 const Navbar = () => {
   return (
     <div className='bg-[#121212] w-screen flex gap-2 items-center'>
-        <img className='w-10 ml-5' src={logo} alt="" />
-        <h1 >Safe wallet</h1>
-        <img className='w-10 rounded-full absolute right-10' src={user} alt="" />
+        <img className='w-10 ml-5 cursor-pointer' src={logo} alt="" />
+        <h1 className='font-bold cursor-pointer' >Safe wallet</h1>
+        <h1 className='absolute right-28 cursor-pointer' >Log out</h1>       
+        <img className='w-10 rounded-full absolute right-10 cursor-pointer' src={user} alt="" />
     </div>
   )
 }
--- src/Components/Slidebar.jsx
+++ src/Components/Slidebar.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const Slidebar = () => {
   return (
-    <div className='bg-[#121212] w-[25%]'>
-        <h1>sliber</h1>
+    <div className='bg-[#121212] w-[20%] block  '>
+        <div className='bg-[#e2e8f0] text-black w-[60%] h-[7%] rounded-md  mt-7 ml-14 cursor-pointer'>
+            <h1 className='text-center pt-2.5 font-bold'>Dashboad</h1>
+        </div>
+        <div className='bg-[#e2e8f0] text-black w-[60%] h-[7%] rounded-md m-7 ml-14 cursor-pointer'>
+            <h1 className='text-center pt-2.5 font-bold'>Wallet</h1>
+        </div>
+        <div className='bg-[#e2e8f0] text-black w-[60%] h-[7%] rounded-md m-7 ml-14 cursor-pointer'>
+            <h1 className='text-center pt-2.5 font-bold'>Acadamy</h1>
+        </div>
+        <div className='bg-[#e2e8f0] text-black w-[60%] h-[7%] rounded-md m-7 ml-14 cursor-pointer'>
+            <h1 className='text-center pt-2.5 font-bold'>Play</h1>
+        </div>
+        <div className='bg-[#e2e8f0] text-black w-[60%] h-[7%] rounded-md m-7 ml-14 cursor-pointer'>
+            <h1 className='text-center pt-2.5 font-bold'>Finecare</h1>
+        </div>
+        <div className='bg-[#e2e8f0] text-black w-[60%] h-[7%] rounded-md m-7 ml-14 cursor-pointer'>
+            <h1 className='text-center pt-2.5 font-bold'>Network</h1>
+        </div>
+        
     </div>
   )
 }
